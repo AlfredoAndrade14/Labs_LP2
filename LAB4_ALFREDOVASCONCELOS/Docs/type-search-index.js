b942caf7d515de8d9bdd94a17667dec9f77c7b48
Alfredo Vasconcelos de Andrade <alfredo.andrade@ccc.ufcg.edu.br>
2022-11-18 19:57:02 -0300

    Delete LAB4_ALFREDOVASCONCELOS/Docs directory

--- LAB4_ALFREDOVASCONCELOS/Docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"controle","l":"Aluno"},{"p":"controle","l":"AlunoTest"},{"p":"controle","l":"ControleAlunos"},{"p":"controle","l":"ControleTest"},{"p":"controle","l":"Grupo"},{"p":"controle","l":"GrupoTest"},{"p":"controle","l":"Main"}];updateSearchResults();
